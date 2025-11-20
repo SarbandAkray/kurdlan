364f2c8c65775d58d7f8df298d4143add05371a7
sarband <81394460+SarbandAkray@users.noreply.github.com>
2025-11-21 02:23:17 +0300

    menu added for the no arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, CMake!" << std::endl;
-    system("pause"); 
-    
+int main(int argc, char* argv[]) {
+
+    // check if no arguments are provided
+    if (argc < 2) {
+        std::cout << "Zmany Barnama Sazy Kurdlan" << std::endl;
+        
+        std::cout << "Bakarhenan:" << std::endl;
+
+        std::cout << "kurdlan " << "drustkrdn <nawi proja>  - drust krdni proja" << std::endl;
+        std::cout << "kurdlan " << "karkrdn - jebajekrdny proja" << std::endl;
+        std::cout << "kurdlan " << "amadakrdn - amadakrdni proja" << std::endl;
+        std::cout << "kurdlan " << "xawankrdn - xawaenkrdni  proja" << std::endl;
+
+        system("pause");
+
+        return 0;
+    }
+
+    std::string firstArg = argv[1];
+
+    system("pause");
+
     return 0;
 }
